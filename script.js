fa42a15719d497652161af19b67b7f0f9048c42c
Rodrigo9712 <04727413060@senacrs.edu.br>
2025-12-05 19:05:57 -0300

    feat(js): adiciona alternância entre receitas com botão

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 echo 'function mostrarReceita() { document.getElementById("receita").innerText = "Pizza Calabresa"; }' > script.js
 
 echo "function mostrarReceita() { document.getElementById('receita').innerText = 'Pastel de Carne'; }" > script.js
+
+echo "let receitas = ['Pizza Calabresa', 'Pastel de Carne', 'Xis de Salada']; let index = 0; function mostrarReceita() { document.getElementById('receita').innerText = receitas[index]; index = (index + 1) % receitas.length; }" > script.js
