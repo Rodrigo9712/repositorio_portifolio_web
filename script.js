2c046302741bc8d4d49b9f2f9142f65c588396db
Rodrigo9712 <04727413060@senacrs.edu.br>
2025-12-05 19:05:17 -0300

    feat(js): altera função para mostrar receita 2

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 echo 'function mostrarReceita() { document.getElementById("receita").innerText = "Pizza Calabresa"; }' > script.js
+
+echo "function mostrarReceita() { document.getElementById('receita').innerText = 'Pastel de Carne'; }" > script.js
